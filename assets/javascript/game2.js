1d991319f4152d97f431a08399658ac765b0d82e
shirinR <shirin.raad@gmail.com>
2017-08-12 02:09:57 -0700

    edit game2.js

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,90 +1,115 @@
 window.onload = function() {
+  var totalScore = 0
+  var finished = false;
 
-  $('#computerGuess').html('<strong>Computer guess: ' + crystalGame.computerGuess + '</strong>');
+  var crystalGame = {
+    wins: 0,
+    losses: 0,
+
+    computerGuess: function(){
+      return crystalGame.randomIntFromInterval(19,120);
+    },
+    crystal1: function(){
+      return crystalGame.randomIntFromInterval(1,12);
+    },
+    crystal2: function(){
+      return crystalGame.randomIntFromInterval(1,12);
+    },
+    crystal3: function(){
+      return crystalGame.randomIntFromInterval(1,12);
+    },
+    crystal4: function(){
+      return crystalGame.randomIntFromInterval(1,12);
+    },
+    randomIntFromInterval: function(min,max){
+      return Math.floor(Math.random()*(max-min+1)+min);
+    },
+
+    resetvalue: function(){
+      console.log('RESET!!!!!');
+      crystalGame.totalScore = 0;
+      crystalGame.wins = 0;
+      crystalGame.losses = 0;
+      computerGuess = crystalGame.randomIntFromInterval(19,120);
+      crystalNum1 = crystalGame.crystal1();
+      crystalNum2 = crystalGame.crystal2();
+      crystalNum3 = crystalGame.crystal3();
+      crystalNum4 = crystalGame.crystal4();
+      $("#status").html('<p><strong>Status >> </strong></p>');
+      $("#wins").html('<p>Wins: 0</p>');
+      $("#losses").html('<p>Losses: 0</p>');
+      $("#guessValue").html('<p id="guessValue"><strong>0</strong></p>');
+      $('#computerGuess').html('<strong><h3>Computer guess: ' + crystalGame.computerGuess() + '</h3></strong>');
+      finished = false;
+    },
+
+    compareResults: function(){
+      if (totalScore === crystalGame.computerGuess()) {
+        crystalGame.wins++;
+        $("#wins").html('<p>Wins: ' + crystalGame.wins + '</p>');
+        $("#status").html('<p ><strong>Status >> WIN!!!! </strong></p>');
+        finished = true
+        setTimeout(function () {
+          crystalGame.resetvalue();
+        }, 3000);
+        alert('WIN!!!!!')
+
+      } else if (totalScore > crystalGame.computerGuess()){
+
+        crystalGame.losses++;
+        $("#losses").html('<p>Losses: ' + crystalGame.losses + '</p>');
+        $("#status").html('<p><strong>Status >> LOST!!!! </strong></p>');
+        // alert('LOST!!!!!')
+        finished = true
+        setTimeout(function () {
+          crystalGame.resetvalue();
+        }, 3000);
+        alert('LOST!!!!!')
+        
+      } else {
+        console.log('Keep guessing!');
+      }
+    }
+
+  }
+
+  $('#computerGuess').html('<strong>Computer guess: ' + crystalGame.computerGuess() + '</strong>');
   $("#crystal1").click(function(){
+    if (finished) {
+      return
+    }
     totalScore += crystalNum1;
     $("#guessValue").html('<p id="guessValue"><strong><h1>' + totalScore+ '</h1></strong></p>');
-    compareResults();
+    crystalGame.compareResults();
   });
   $("#crystal2").click(function(){
+    if (finished) {
+      return
+    }
     totalScore += crystalNum2;
     $("#guessValue").html('<p id="guessValue"><strong><h1>' + totalScore+ '</h1></strong></p>');
-    compareResults();
+    crystalGame.compareResults();
   });
   $("#crystal3").click(function(){
+    if (finished) {
+      return
+    }
     totalScore += crystalNum3;
     $("#guessValue").html('<p id="guessValue"><strong><h1>' + totalScore+ '</h1></strong></p>');
-    compareResults();
+    crystalGame.compareResults();
   });
   $("#crystal4").click(function(){
+    if (finished) {
+      return
+    }
     totalScore += crystalNum4;
     $("#guessValue").html('<p id="guessValue"><strong><h1>' + totalScore+ '</h1></strong></p>');
-    compareResults();
+    crystalGame.compareResults();
   });
-};
 
-  var totalScore = 0
   var crystalNum1 = crystalGame.crystal1();
   var crystalNum2 = crystalGame.crystal2();
   var crystalNum3 = crystalGame.crystal3();
   var crystalNum4 = crystalGame.crystal4();
-
-  var crystalGame = {
-  wins: 0,
-  losses: 0,
-
-  computerGuess: function(){
-    crystalGame.randomIntFromInterval(19,120);
-  },
-  crystal1: function(){
-    crystalGame.randomIntFromInterval(1,12);
-  },
-  crystal2: function(){
-    crystalGame.randomIntFromInterval(1,12);
-  },
-  crystal3: function(){
-    crystalGame.randomIntFromInterval(1,12);
-  },
-  crystal4: function(){
-    crystalGame.randomIntFromInterval(1,12);
-  },
-  randomIntFromInterval: function(min,max){
-    Math.floor(Math.random()*(max-min+1)+min);
-  },
-
-  reset: function(){
-    console.log('RESET!!!!!');
-    totalScore = 0;
-    wins = 0;
-    losses = 0;
-    computerGuess = randomIntFromInterval(19,120);
-    crystalNum1 = crystal1();
-    crystalNum2 = crystal2();
-    crystalNum3 = crystal3();
-    crystalNum4 = crystal4();
-    $("#status").html('<p><strong>Status >> </strong></p>');
-    $("#wins").html('<p>Wins: 0</p>');
-    $("#losses").html('<p>Losses: 0</p>');
-    $("#guessValue").html('<p id="guessValue"><strong>0</strong></p>');
-    $('#computerGuess').html('<strong><h3>Computer guess: ' + compareResults.computerGuess + '</h3></strong>');
-  },
-
-  compareResults: function(){
-    if (totalScore === compareResults.computerGuess) {
-      wins++;
-      $("#wins").html('<p>Wins: ' + compareResults.wins + '</p>');
-      $("#status").html('<p ><strong>Status >> WIN!!!! </strong></p>');
-      alert('WIN!!!!!')
-      compareResults.resetvalue;
-    } else if (totalScore > computerGuess){
-      losses++;
-      $("#losses").html('<p>Losses: ' + compareResults.losses + '</p>');
-      $("#status").html('<p><strong>Status >> LOST!!!! </strong></p>');
-      alert('LOST!!!!!')
-      compareResults.resetvalue;
-    } else {
-      console.log('Keep guessing!');
-    }
-  },
 
 };
